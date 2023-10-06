e26ff995ab6a2cb25eff95031ecedebbf4e11ac1
Muhammed Sinan CP <cpmuhammedsinan@gmail.com>
2023-10-06 17:24:41 +0530

    CP Restaurants App Created

---
+++ android/app/src/main/kotlin/com/example/cp_restaurants/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cp_restaurants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:cp_restaurants/view/on_boarding/on_boarding_view.dart';
+import 'package:flutter/material.dart';
+
+import 'common/color_extension.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          primaryColor: TColor.primary,
+          fontFamily: "Quicksand"),
+      home: OnBoardingView(),
+    );
+  }
+}
