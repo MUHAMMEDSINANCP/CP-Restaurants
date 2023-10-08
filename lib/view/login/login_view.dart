f2fa94f1e21fc6647d825932f74604f6b5b3ca51
Muhammed Sinan CP <cpmuhammedsinan@gmail.com>
2023-10-08 21:49:03 +0530

    just now macha

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -1,8 +1,1 @@
 //Login
-qwelrjweikqhjfikowheikolfhldawsehfol[ihsdl;'dhnfulwheo[fuhASDUOLhfouasdghfpousaegbpufvgbaspdbcvasdv
-dfasdjfvvbiyawsdvbhwef
-]sadopfhiowbhwf
-p[]qwhfuogsd
-]ioaehfoawsef
-]awe9psfyowehf
-[qweyfiowfw[]wqfywo[aefhawe]]']
